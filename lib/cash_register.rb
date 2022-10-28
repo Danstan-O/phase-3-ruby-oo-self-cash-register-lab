10f9716ab316c9bda55a243a96ff834cce0341ff
danstan <danstan.oduor@student.moringaschool.com>
2022-10-28 18:48:45 +0300

    cash register

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,59 @@
+require "pry"
 
+class CashRegister
+    attr_accessor :total, :discount, :title, :price
+    
+
+
+    def initialize(discount=0)
+        @discount=discount
+        @total=0 
+        @items=[]
+        
+    end
+    def add_item(title, price, quantity=1)
+        @title=title
+        @price=price
+        @items << {title: title, price: price, quantity: quantity}
+        @total += price * quantity
+        
+    end
+    def apply_discount
+        if (@discount>0)
+          @total=@total*(100-self.discount)/100
+         "After the discount, the total comes to $#{@total}."
+                
+            else
+                "There is no discount to apply."
+            end
+        end
+
+
+        def items
+            name_array = Array.new
+
+        @items.each do |item|
+            item[:quantity].times {|| name_array << item[:title]}
+        end
+
+        name_array
+        end
+       
+        def void_last_transaction
+            @items.delete(-1)
+            @total -= (@items[-1][:price]).to_f * (@items[-1][:quantity]).to_f
+            @total
+        end
+
+end
+
+
+
+total=CashRegister.new
+
+
+
+
+
+
+binding.pry
